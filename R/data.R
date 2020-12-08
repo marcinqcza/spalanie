96b7edbb5d9ea3ff4c4907e88380e31b3e27af7f
marcinqcza <max645@interia.pl>
2020-12-08 08:44:48 +0100

    poprawki

--- R/data.R
+++ R/data.R
@@ -1,10 +1,9 @@
 #' Plik do wczytywania danych zewnetrznych potrzebnych do dzialania pakietu
 #'
 #' @source Plik Wskazniki zawiera dane emisji z transportu drogowego EEA - Europejska Agencja Srodowiska
+#'
 #' @format Ramka danych zawiera:
 #' @examples
-#' \dontrun{
-#' dane_wej
-#' }
+#' \dontrun
 "input"
 "wskazniki"
